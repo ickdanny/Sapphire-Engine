205097ab40d3ce672a175b3bd22690404c37b660
ickdanny <fake_email@gmail.com>
2023-09-10 21:33:02 -0400

    create Constructure ArrayList

--- source/AAAgame/main.c
+++ source/AAAgame/main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include "ZMath.h"
 
+#include <Constructure_ArrayList.h>
+
+CONSTRUCTURE_ARRAYLIST(IntArrayList, intArrayList, int)
+
+void printInt(int toPrint){
+    printf("%d\n", toPrint);
+}
+
 int main(){
-    Point point = {1.0f, 2.0f};
-    point = pointCopy(point);
-    char string[20] = {0};
-    pointToString(point, string, 20);
-    printf("%s\n", string);
-    Vector vector = {3.4f, 4.5f};
-    vector = vectorCopy(vector);
-    vectorToString(vector, string, 20);
-    printf("%s\n", string);
-    Polar polar = {4.0f, 90.0f};
-    polar = polarCopy(polar);
-    polarToString(&polar, string, 20);
-    printf("%s\n", string);
+    IntArrayList intArrayList = intArrayListMake(3);
+    intArrayListPushBackValue(&intArrayList, 1);
+    intArrayListPushBackValue(&intArrayList, 2);
+    intArrayListPushBackValue(&intArrayList, 3);
+    intArrayListPushBackValue(&intArrayList, 4);
+    intArrayListPushBackValue(&intArrayList, 5);
+    intArrayListPopBack(&intArrayList);
+    intArrayListErase(&intArrayList, 1);
+    intArrayListAssignValue(&intArrayList, 12, 1);
+    intArrayListInsertValue(&intArrayList, 222, 3);
+    intArrayListInsertValue(&intArrayList, -23, 0);
+    for(int i = 0; i < 100; ++i){
+        intArrayListPushBackValue(&intArrayList, i);
+    }
+    intArrayListErase(&intArrayList, 2);
+
+    intArrayListForEachValue(&intArrayList, printInt);
+    printf("Size : %d\n", intArrayList.size);
+    printf("Capacity : %d\n", intArrayList.capacity);
+    printf("Back: %d\n", intArrayListBack(&intArrayList));
+    printf("Front: %d\n", intArrayListFront(&intArrayList));
+
+    intArrayListFree(&intArrayList);
+    printf("Size : %d\n", intArrayList.size);
+    printf("Capacity : %d\n", intArrayList.capacity);
 }
