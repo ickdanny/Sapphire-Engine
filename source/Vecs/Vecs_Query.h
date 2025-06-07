65d36f1bc286d182c063afbda05bac263b94d84a
ickdanny <fake_email@gmail.com>
2025-06-07 18:53:07 -0400

    finished interace for vecs query

--- source/Vecs/Vecs_Query.h
+++ source/Vecs/Vecs_Query.h
@@ -21,8 +21,112 @@ typedef struct VecsQuery{
 
     /* internal value for tracking modifications */
     size_t _modificationCount;
-
-    //todo: hasrejectcomponentset can just compare to 0
 } VecsQuery;
+
+/*
+ * Constructs and returns a new query with the
+ * specified accept and reject component sets;
+ * iterates over the given archetype list and accepts
+ * all fitting archetypes automatically
+ */
+VecsQuery vecsQueryMake(
+    VecsComponentSet acceptComponentSet,
+    VecsComponentSet rejectComponentSet,
+    ArrayList *archetypeListPtr
+);
+
+/*
+ * Adds the specified archetype into the given query
+ * if it fits the query restrictions, otherwise does
+ * nothing; returns true if the archetype was accepted,
+ * false otherwise
+ */
+bool _vecsQueryTryAcceptArchetype(
+    VecsQuery *queryPtr,
+    size_t archetypeIndex
+);
+
+/*
+ * Frees all memory associated with the specified
+ * query
+ */
+void vecsQueryFree(VecsQuery *queryPtr);
+
+/*
+ * Iterates over the entities that fit a given query
+ */
+typedef struct VecsQueryItr{
+    VecsQuery *_queryPtr;
+    /*
+     * index of the current archetype being iterated
+     * in the query's archetype index list (not the
+     * global archetype list)
+     */
+    size_t _currentArchetypeIndex;
+
+    /* the iterator over the current archetype */
+    _VecsArchetypeItr _archetypeItr;
+
+    size_t _storedModificationCount;
+} VecsQueryItr;
+
+/*
+ * Returns true if the specified query iterator
+ * has more elements, false otherwise
+ */
+bool vecsQueryItrHasEntity(VecsQueryItr *itrPtr);
+
+/*
+ * Advances the query itr to point to the next entity
+ */
+void vecsQueryItrAdvance(VecsQueryItr *itrPtr);
+
+/*
+ * Returns a pointer to the component specified by
+ * the given component id of the entity currently being
+ * pointed to by the given query iterator; error
+ * if the component id is invalid; returns NULL if the
+ * component is a marker
+ */
+void *_vecsQueryItrGetPtr(
+    VecsQueryItr *itrPtr,
+    VecsComponentId componentId
+);
+
+/*
+ * Returns a pointer to the specified component of the
+ * entity currently being pointed to by the given
+ * query iterator; error if the query does not accept
+ * the component in question; returns NULL if the
+ * component is a marker
+ */
+#define vecsQueryItrGetPtr( \
+    typeName, \
+    itrPtr \
+) \
+    ((typeName*)_vecsQueryItrGetPtr( \
+        itrPtr, \
+        vecsComponentGetId(typeName) \
+    ))
+
+/*
+ * Returns the specified component of the entity
+ * currently being pointed to by the given query
+ * iterator; error if the query does not accept the
+ * component in question; should not be used for
+ * marker components
+ */
+#define vecsQueryItrGet( \
+    typeName, \
+    itrPtr \
+) \
+    ( \
+        (*vecsQueryItrGetPtr( \
+            typeName, \
+            itrPtr \
+        )) \
+    )
+
+/* query itr does not need to be freed */
 
 #endif
